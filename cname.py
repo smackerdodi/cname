032b9efc98992c59f64f344baa069c65e0f230cb
m_elbadry <melbadry9@github.com>
2019-05-18 17:19:47 +0200

    adding multithreading support

--- cname.py
+++ cname.py
@@ -1,39 +1,69 @@
 import sys
-import dns
-import dns.resolver
-import colorama
 import time
-from colorama import Fore, Style
-from requests.exceptions import ConnectionError
-print(Fore.WHITE + '''/
+import queue
+import threading
 
+import dns.resolver
+import win_unicode_console
+from colorama import Fore, Style, init
+
+#introduction
+print(Fore.WHITE + '''/
                           _                _           _ _ 
  ___ _ __ ___   __ _  ___| | _____ _ __ __| | ___   __| (_)
 / __| '_ ` _ \ / _` |/ __| |/ / _ \ '__/ _` |/ _ \ / _` | |
 \__ \ | | | | | (_| | (__|   <  __/ | | (_| | (_) | (_| | |
 |___/_| |_| |_|\__,_|\___|_|\_\___|_|  \__,_|\___/ \__,_|_|
-
 ''')
 print(Fore.GREEN + '''/              CODED BY : DAOUD YOUSSEF          
-
-
 ''')                                              
 print (Fore.RED + 'PREPARING FOR BEGIN SCRIPT')
 for i in range(5,0,-1):
     time.sleep(1)
-    print(str(i) + i * " . ")
-sublist=sys.argv[1]
+print(str(i) + i * " . ")
+
+
+#enable coloring on win
+try:
+	win_unicode_console.enable()
+	init()
+except:
+	pass
+
+#queue and lock var
+domains = queue.Queue()
+lock = threading.Lock()
+
+# reading file
+sublist = sys.argv[1]
+subfile = open(sublist, 'r')
+
+#setting dns resolver
 my_resolver = dns.resolver.Resolver()
 my_resolver.nameservers = ['8.8.8.8']
-subfile=open(sublist, 'r')
+
+#populate queue with domains
 for sub in subfile:
-	try:
-		subd=sub.strip()		
-		answer=my_resolver.query(sub.strip(), 'CNAME')
-		for data in answer:
-			print(Fore.WHITE + (subd))
-			print(Fore.GREEN +'C_NAME is :'+ Fore.YELLOW + str(data))
-	except :    
-   		print(Fore.WHITE + (sub) + Fore.RED + (' has no cname'))
+	domains.put(sub.strip())
+
 subfile.close()
 
+#checking cname
+def Check(domain):
+	try:
+		answer=my_resolver.query(domain, 'CNAME')
+		for data in answer:
+			with lock:
+				print(Fore.WHITE + (domain))
+				print(Fore.GREEN +'C_NAME is :'+ Fore.YELLOW + str(data))
+	except:
+		with lock:
+			print(Fore.WHITE + (domain) + Fore.RED + (' has no cname'))
+	domains.task_done()
+
+#starting threads
+while not domains.empty():
+	threading.Thread(target=Check,args=(domains.get(),)).start()
+
+#wait until all threads done
+domains.join()
