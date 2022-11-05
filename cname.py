cb6b94b572f75a492dc1ef7396d16f16773145c2
smackerdodi2 <36116795+smackerdodi@users.noreply.github.com>
2022-11-05 18:55:01 +0200

    Update cname.py

--- cname.py
+++ cname.py
@@ -5,7 +5,7 @@ import colorama
 import time
 from colorama import Fore, Style
 from requests.exceptions import ConnectionError
-print(Fore.WHITE + '''/
+print(Style.BRIGHT + Fore.WHITE + '''/
 
                           _                _           _ _ 
  ___ _ __ ___   __ _  ___| | _____ _ __ __| | ___   __| (_)
@@ -18,22 +18,31 @@ print(Fore.GREEN + '''/              CODED BY : DAOUD YOUSSEF
 
 
 ''')                                              
-print (Fore.RED + 'PREPARING FOR BEGIN SCRIPT')
+print (Style.BRIGHT + Fore.RED + 'PREPARING FOR BEGIN SCRIPT')
 for i in range(5,0,-1):
     time.sleep(1)
     print(str(i) + i * " . ")
 sublist=sys.argv[1]
+outlist=sys.argv[2]
 my_resolver = dns.resolver.Resolver()
 my_resolver.nameservers = ['8.8.8.8']
 subfile=open(sublist, 'r')
+outfile=open(outlist, 'a')
+numberofsubd = sum(1 for line in open(sublist))
+print(Style.BRIGHT + Fore.CYAN + "Number of Subdomains = " + str(numberofsubd))
+line = 0
 for sub in subfile:
+	line+=1
 	try:
-		subd=sub.strip()		
-		answer=my_resolver.query(sub.strip(), 'CNAME')
+		subd=sub.strip()
+		answer=my_resolver.resolve(sub.strip(), 'CNAME')
 		for data in answer:
-			print(Fore.WHITE + (subd))
-			print(Fore.GREEN +'C_NAME is :'+ Fore.YELLOW + str(data))
+			c=subd+ " : " + str(data)
+			print(Style.BRIGHT + Fore.WHITE + (subd))
+			print(Style.BRIGHT + Fore.GREEN +'C_NAME is :'+ Fore.YELLOW + str(data))
+			outfile.write(c+"\n")
 	except :    
-   		print(Fore.WHITE + (sub) + Fore.RED + (' has no cname'))
+   		print(Style.BRIGHT + Fore.RED + '\x1b[2K' + str(line) + "|" + str(numberofsubd) , end='\r')
 subfile.close()
+
 
